44a247ab229a4d53bd8e5964be1f0cde16c31ed6
Athly23 <andrewly06@gmail.com>
2020-04-16 14:19:42 -0400

    CRUD done

--- Connection.js
+++ Connection.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/Chuck-Norris-API", {
   useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
 
 module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const app = require("express")();
 const Chuck = require("./models/Chuck.js");
+const parser = require("body-parser");
+
+app.use(parser.json());
 
 app.get("/", (req, res) => {
   Chuck.find({}).then(jokes => {
     res.json(jokes);
   });
 });
+
+app.post("/", (req, res) => {
+  Chuck.create(req.body).then(jokes => {
+    res.json(jokes);
+  });
+});
+
+app.put("/list/:id", function (req, res) {
+  Chuck.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(
+    jokes => {
+      res.json(jokes);
+    }
+  );
+});
+
+app.delete("/:id", function (req, res) {
+  Word.findOneAndRemove({ _id: req.params.id }).then(jokes => {
+    res.json(jokes);
+  });
+});
+
+// app.get("/:id", (req, res) => {
+//   Chuck.findById(req.params.id).then(jokes => {
+//     res.json(jokes);
+//   });
+// });
 
 app.listen(4000, () => {
   console.log("I'm on 4000");
