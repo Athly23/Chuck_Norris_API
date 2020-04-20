870aae4fac0b9803942988423abc294529b56530
Athly23 <andrewly06@gmail.com>
2020-04-20 01:48:41 -0400

    some minor changes

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const parser = require("body-parser");
 
 app.use(parser.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); //change to url for deployed site
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //this route gets all the jokes
 app.get("/jokes", (req, res) => {
   Chuck.find({}).then(jokes => {
@@ -13,7 +23,7 @@ app.get("/jokes", (req, res) => {
 
 //this route creates a joke
 app.post("/jokes", (req, res) => {
-  console.log(req.body.value);
+  console.log(req);
   Chuck.find({}).then(jokes => {
     let length = jokes.length;
     let jokeID = length + 1;
@@ -39,6 +49,7 @@ app.delete("/jokes/:id", (req, res) => {
   });
 });
 
+//this route get single joke by ID
 app.get("/jokes/:id", (req, res) => {
   Chuck.find({ jokeID: req.params.id }).then(jokes => {
     res.json(jokes);
