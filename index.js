955e8646a90bd89215377c9f010489ed1d3c0ec2
Athly23 <andrewly06@gmail.com>
2021-03-02 09:18:14 -0500

    fixed bugs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(parser.json());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); //change to url for deployed site
+  res.header("Access-Control-Allow-Origin", "*"); //http://localhost:3000//change to url for deployed site
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
